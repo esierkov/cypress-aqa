0451b286bc6a247036a905c011f7baefaa268629
Yevhen Sierkov <esierkov@gmail.com>
2026-07-09 18:38:04 +0200

    first autotest with cypress queries

---
+++ cypress/pages/HomePage.js
@@ -0,0 +1,21 @@
+export const homePage = {
+
+    header: {
+        logo: '.header_logo',
+        homeLink: 'a.header-link[href="/"]',
+        aboutButton: 'button[appscrollto="aboutSection"]',
+        contactsButton: 'button[appscrollto="contactsSection"]',
+        guestLoginButton: 'button.header-link.-guest',
+        signInButton: 'button.header_signin',
+    },
+
+    footer: {
+        facebookLink: 'a[href*="facebook.com/Hillel.IT.School"]',
+        telegramLink: 'a[href*="t.me/ithillel_kyiv"]',
+        youtubeLink: 'a[href*="youtube.com/user/HillelITSchool"]',
+        instagramLink: 'a[href*="instagram.com/hillel_itschool"]',
+        linkedInLink: 'a[href*="linkedin.com/school/ithillel"]',
+        hillelLink: 'a[href*="https://ithillel.ua"]',
+        supportEmail: '[href*="mailto:developer@ithillel.ua"]'
+    },
+};
